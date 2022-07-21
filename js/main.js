376a16a4dd6beb4724551406c125e4493e04270b
seregov-sd <seregov.sd@yandex.by>
2022-07-21 21:39:09 +0300

    feat: added dropdown for mobile devices to navigation

--- js/main.js
+++ js/main.js
@@ -7,4 +7,21 @@ $(document).ready(function () {
         $('body, html').animate({scrollTop: top}, 400); // плавно переходим к блоку
     });
 
+    var menuButton = $(".menu__button");
+    $(".menu__button").click(function () {
+        if (!$(".menu__button").hasClass("btn--active") && !$(".menu__list").hasClass("menu__list--active")) {
+            $(this).addClass("btn--active");
+            $(".menu__list").addClass("menu__list--active");
+        } else {
+            $(this).removeClass("btn--active");
+            $(".menu__list").removeClass("menu__list--active");
+        }
+    });
+    $(document).click(function (e) {
+        if (!menuButton.is(e.target) && menuButton.has(e.target).length === 0) {
+            $(".menu__button").removeClass("btn--active");
+            $(".menu__list").removeClass("menu__list--active");
+        }
+        ;
+    });
 });
